8c434e5b36e644bcacfb42e9e4993844e32adff1
X <x@Xs-MacBook-Air.local>
2016-01-16 13:41:47 +0100

    modified changeTime.py

--- changeTime.py
+++ changeTime.py
@@ -16,12 +16,9 @@ def getSampleTime(fileName):
     seconds = int(startTime[4] + startTime[5])
 
     minutes = minutes + minutesOffset
-    print(minutes)
     if minutes >= 60:
         offsetHours = int(minutes/60)
         minutes = minutes % 60        
-        print(offsetHours)
-        print(int(minutes/60))
         hours = hours + offsetHours
         if hours >= 24:
             offsetDate = int(hours/24)
@@ -31,15 +28,15 @@ def getSampleTime(fileName):
         hours = str("0") + str(hours)
     if minutes < 10:
         minutes = str("0") + str(minutes)
+    if seconds < 10:
+        seconds = str("0") + str(seconds)
 
     newName = splitList[0] + "_" + splitList[1] + "_" + splitList[2] + "_" + splitList[3] + "_" + str(date) + "_" + splitList[5] + "_" + str(hours) + str(minutes) + str(seconds) + ".npy"
     return newName
 
 
-rootfolder =  "/Volumes/Vogeltjes/resultsb73"
-files = os.listdir(rootfolder)
+path =  "/Volumes/Untitled/b73analyzed"
+files = os.listdir(path)
 for fileName in files:
     newFilename = getSampleTime(fileName)
     os.system("mv " + rootfolder + "/" + fileName + " " + rootfolder + "/" + newFilename)
-
-
